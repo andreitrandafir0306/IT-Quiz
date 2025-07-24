4a0a2a2fead946a0141f45bc2cae01487416c86e
Nickster0306 <andrei.trandafir0306@gmail.com>
2025-07-24 16:32:05 +0000

    Deploy TF infra & test sample S3 website

---
+++ Terraform/main.tf
@@ -0,0 +1,70 @@
+# Create S3 bucket 
+
+resource "aws_s3_bucket" "website" {
+  bucket = "my-it-quiz-123"
+
+  tags = {
+    Name        = "Simple IT Quiz"
+  }
+}
+
+# Remove public access block
+
+resource "aws_s3_bucket_public_access_block" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+# Setup bucket ACL & object ownership 
+
+resource "aws_s3_bucket_ownership_controls" "website" {
+  bucket = aws_s3_bucket.website.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "website" {
+  depends_on = [aws_s3_bucket_ownership_controls.website]
+
+  bucket = aws_s3_bucket.website.id
+  acl    = "public-read"
+}
+
+# Setup bucket access policy 
+
+resource "aws_s3_bucket_policy" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.website.arn}/*"
+      }
+    ]
+  })
+}
+
+# Setup static website
+
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
